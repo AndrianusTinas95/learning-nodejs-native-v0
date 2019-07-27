f4740bb5b18a884fe8e2841a886f32a75bdfdd76
andriTinas <andrianustinas.siskom@gmail.com>
2019-07-27 16:17:32 +0700

    2-pass-because-I-am-using-VSCode-3-Basic-Javascript

---
+++ TestApp1/app.js
@@ -0,0 +1,79 @@
+// var satu     = 10 ;
+// var dua      = 20 ;
+
+// var tiga = satu + dua ;
+// console.log("penjumlahan " + satu + " dan " + dua + " adalah " + tiga)
+
+// var nim = "H1051141029";
+// if(nim == "H1051141029"){
+    //     console.log("Andrianus Tinas");
+    // }else{
+        //     console.log("bukan tinas");
+        // }
+        
+
+// var nim = "H1051141029";
+// switch(nim){
+//     case "H1051141029":
+//         console.log("Tinas");
+//         break;
+//     case "H1051141060":
+//         console.LOG("Deni");
+//         break;
+//     default:
+//         console.log("Tidak dikenali");
+// }
+
+//looping
+
+// looping for 
+// var n = 10;
+// for(var i = 0; i < n ; i++){
+//     console.log("Perulangan ke " + i);
+// }
+
+// looping while
+// var n = 10;
+// var i = 0;
+// while(i<n){
+//     console.log("Perulangan ke  " + i++);
+// }
+
+// array
+// var arr = [20,30,40];
+
+// console.log(arr);
+
+// for(var i =0; i < arr.length ; i++){
+//     console.log(arr[i]);
+// }
+
+// object
+// var json =[
+//     {
+//         name : "Tinas",
+//         age  : "23"
+//     },
+//     {
+//         name : "Uul",
+//         age  : "27"
+//     }
+// ];
+
+// for(var i = 0 ; i <json.length ; i++){
+//     console.log(json[i].name + "  age : " + json[i].age);
+// }
+
+// function
+// function TinasFunction(){
+//     // console.log(" I am a function ");
+//     return "I am a function";
+// }
+
+// TinasFunction();
+// console.log(TinasFunction());
+
+
+
+
+
