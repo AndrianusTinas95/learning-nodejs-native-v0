a811421dca6db52388a74afd167e7cf11e4020f4
andriTinas <andrianustinas.siskom@gmail.com>
2019-07-28 12:06:43 +0700

    5-Object-References

--- TestApp1/app.js
+++ TestApp1/app.js
@@ -1,33 +1,13 @@
-// setTimeout(function(){
-//     console.log("saya ditampilakan denagan jeda 3 detik");
-// },3000);
-// function Order(id){
-//     console.log("ID Order "+id);
-//     ProsesOrder(id);
-// }
+// object reference 
+// nilai induk akan berubah
 
-// function ProsesOrder(idOrder){
-//     setTimeout(function(){
-//         console.log("ID Order "+ idOrder +" Prosessed");
-//     },3000);
-// }
-
-// Order(101);//first exceusion
-// Order(102);//secon exsecution
-// Order(103);//last exsecusion
-
-function Order(id,timeOut){
-    console.log("ID Order "+id);
-    ProsesOrder(id,timeOut);
+var myObj ={
+        name:'Tinas',
+        age:23
 }
 
-function ProsesOrder(idOrder,timeOut){
-    setTimeout(function(){
-        console.log("ID Order "+ idOrder +" Prosessed");
-    },timeOut);
-}
+// console.log(myObj);
+var obj2 = myObj;
+obj2.age = 19;
 
-Order(101,5000);//last exsecusion
-Order(102,2000);//first exsecusion
-Order(103,3000);//secon exsecusion
-
+console.log(myObj);
