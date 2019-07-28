57c427f6f1b74eff27ae9ab3a021f41a6eabe32e
andriTinas <andrianustinas.siskom@gmail.com>
2019-07-28 15:26:31 +0700

    11-Simple-Web-file-Server

--- TestApp1/app.js
+++ TestApp1/app.js
@@ -1,14 +1,24 @@
-var http = require("http");
+var http    = require("http");
+var fs      = require("fs");
 
 http.createServer(function(req,res){
-   if(req.url  != "/favicon.ico"){
-       console.log(req.url);
-       res.writeHead(200,{"Content-Type" : "text/plain"});
-       res.write("Hallo from Node Js Server");
-       res.write("You Request " + req.url);
-       res.end();
-   }
-
+        var kode = 0;
+        var file = "";
+        
+        if(req.url == "/"){
+            kode = 200; 
+            file = 'index.html'; 
+        }else if(req.url == "/contact"){
+            kode = 200; 
+            file = 'contact.html'; 
+            
+        }else{
+            // 404
+            kode = 404; 
+            file = '404.html'; 
+        }
+        res.writeHead(kode,{"Content-Type" : "text/html"});
+        fs.createReadStream('./TestApp1/template/'+file).pipe(res);
 }).listen(8888);
 
 console.log("Server is running....");
