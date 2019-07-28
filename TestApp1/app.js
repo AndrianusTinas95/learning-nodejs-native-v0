32ab14dead6da116da9fa1fb442bb85d6485d236
andriTinas <andrianustinas.siskom@gmail.com>
2019-07-28 18:55:43 +0700

    16-Operasi-database-My-SQL

--- TestApp1/app.js
+++ TestApp1/app.js
@@ -2,14 +2,75 @@ var http    = require("http");
 var url     = require("url");
 var router  = require("routes")();
 var view    = require("swig");
+var mysql   = require('mysql');
 
+var conn = mysql.createConnection({
+    host:'localhost',
+    poet:3306,
+    database:"learn_nodejs",
+    user:"root",
+    password:""
+});
+
+// router.addRoute('/',function(req,res){
+//     var html =view.compileFile('./TestApp1/template/index.html')({
+//         title :"index",
+//         data : "ini data"
+//     });
+//     res.writeHead(200,{"Cotent-Type":"text/html"});
+//     res.end(html);  
+// });
 router.addRoute('/',function(req,res){
-    var html =view.compileFile('./TestApp1/template/index.html')({
-        title :"index",
-        data : "ini data"
+    conn.query("select * from mahasiswa",function(err,rows,field){
+        if(err) throw err;
+      
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end(JSON.stringify(rows));
     });
-    res.writeHead(200,{"Cotent-Type":"text/html"});
-    res.end(html);  
+});
+
+router.addRoute('/insert',function(req,res){
+    
+    conn.query("insert into mahasiswa set ?",{
+        nim:"H1051141013",
+        nama:" lagi",
+        alamat:"tes  lagi"
+    },function(err,field){
+        if(err) throw err;
+
+        console.log(field.affectedRows);
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end(JSON.stringify(field.affectedRows));
+    });
+    
+});
+
+router.addRoute('/update',function(req,res){
+    
+    conn.query("update mahasiswa set ? where ?",[
+        {nama:"nanas"},
+        {nim:"h1051141029"}
+    ],function(err,field){
+        if (err) throw err;
+
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end(field.affectedRows+ " updated");
+    })
+
+});
+
+router.addRoute('/delete',function(req,res){
+    
+    conn.query("delete from mahasiswa where ?",{
+        nim:"h1051141029"
+    },function(err,field) {
+        if( err) throw err;
+
+        res.writeHead(200,{"Content-Type":"text/plain"});
+        res.end(field.affectedRows + " delete");
+
+    });
+
 });
 
 router.addRoute('/contact',function(req,res){
