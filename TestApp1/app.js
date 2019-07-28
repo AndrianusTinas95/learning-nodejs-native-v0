ea4c2a0a9767f0c7e39e3c778d91f1bf6d490978
andriTinas <andrianustinas.siskom@gmail.com>
2019-07-28 13:01:55 +0700

    10-Basic-Server

--- TestApp1/app.js
+++ TestApp1/app.js
@@ -1,4 +1,14 @@
+var http = require("http");
 
-require('./andri.js'); 
-console.log("--------------------");
-require('./tinas.js'); 
+http.createServer(function(req,res){
+   if(req.url  != "/favicon.ico"){
+       console.log(req.url);
+       res.writeHead(200,{"Content-Type" : "text/plain"});
+       res.write("Hallo from Node Js Server");
+       res.write("You Request " + req.url);
+       res.end();
+   }
+
+}).listen(8888);
+
+console.log("Server is running....");
